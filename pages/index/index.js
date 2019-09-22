ab636e43ba25c6ac3888587c7908359cabc714a9
李小姐 <1121024033@qq.com>
2019-09-22 18:18:01 +0800

    首页轮播图完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,34 @@
-
-Page({})
+// 首页
+Page({
+    data:{
+        // 轮播图数组
+        swiperList:[],
+    },
+    onLoad(){
+        // 钩子函数=>一开始就获取
+        // 调用获取轮播图数组
+        this.getSwiperData()
+    },
+    // 获取轮播图数据
+    getSwiperData(){
+        // 获取接口数据=>轮播图
+        wx.request({
+            url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+            // 无数据
+            // data: {},
+            // 不需要请求头
+            // header: {'content-type':'application/json'},
+            method: 'GET',
+            // 类型默认值
+            // dataType: 'json',
+            // 文本默认值
+            // responseType: 'text',
+            success: (result) => {
+                // console.log(result);
+                this.setData({
+                    swiperList : result.data.message
+                })
+            }
+        });
+    }
+})
