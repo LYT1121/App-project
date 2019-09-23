fb7400cfff347b7a329be74fa17f8c046bcb5595
李小姐 <1121024033@qq.com>
2019-09-23 10:54:42 +0800

    调用小程序内置的loading效果

--- request/index.js
+++ request/index.js
@@ -1,5 +1,13 @@
+// 设置一个检测请求次数
+let requestTimes = 0
 // 封装ajax请求
 export const request=(params)=>{
+    requestTimes++
+    // 全局的loading效果
+    wx.wx.showLoading({
+        title: "加载中",
+        mask: true,// 遮罩层，在加载完成之前用户无法点击里面的内容
+    });
     //公共的url
     const baseUrl = "https://api.zbztb.cn/api/public/v1"
     return new Promise((resolve,reject)=>{
@@ -14,6 +22,12 @@ export const request=(params)=>{
             fail: (err) => {
                 // 失败之后的回调
                 reject(err)
+            },
+            // 请求回来之后触发
+            complete:()=>{
+                requestTimes--;
+                // 判断请求全部回来之后，让loading消失
+                requestTimes === 0 && wx.hideLoading();
             }
         });
           
