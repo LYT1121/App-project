b529e3d3041520a9d99425a041cab633d02e0c4d
李小姐 <1121024033@qq.com>
2019-09-23 17:51:48 +0800

    分类页面获取数据的时候加一个缓存效果

--- pages/category/index.js
+++ pages/category/index.js
@@ -16,8 +16,34 @@ Page({
   // 定义一个全局数据变量(页面中用不到的在data外设置)=>接口的返回值
   Datas:[],
   onLoad(){
+    this.getDatas()
     // 调用获取分类数据
-    this.getCategoriesData()
+    // this.getCategoriesData()
+  },
+  // 从缓存中的数据中获取接口的数据 
+  getDatas(){
+    // 获取本地存储中的数据=>如果没有就是空值
+    const getDatas = wx.getStorageSync('getlist');
+    // 判断有没有旧的数据
+    if(getDatas){
+      // 表示本地存储有缓存的数据
+      // 判断数据是否已经过期 => 定1分钟时间过期
+      if(Date.now()-getDatas.time>1000*60){
+        // 表示过期=>重新发起请求获取数据
+        this.getCategoriesData()
+      }else{
+        // 把本地存储的数据展示
+        this.Datas = getDatas.data;
+        // 把左右侧的数据展示
+        this.setData({
+          menuList : this.Datas.map(v=>v.cat_name),
+          goodsList : this.Datas[0].children
+        })
+      }
+    }else{
+      // 表示没有数据=>重新发起请求获取数据
+      this.getCategoriesData()
+    }
   },
   // 获取分类数据
   getCategoriesData(){
@@ -27,6 +53,14 @@ Page({
     }).then(result=>{
       // console.log(result);
       this.Datas = result.data.message,
+      // 把获取成功后的数据存入本地存储=>同步存储
+      wx.setStorageSync('getlist', {
+        data:this.Datas,
+        // 时间蹉=>单位毫秒
+        time:Date.now()
+      });
+        
+
       this.setData({
         // categoriesList: result.data.message=>总的数据
         // map遍历数组的方法
